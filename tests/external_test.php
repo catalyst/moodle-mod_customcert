62615cd243b7b69f6cf628a2c089ccd732c31bfc
Noemie Ariste <noemie.ariste@catalyst.net.nz>
2025-04-04 12:49:06 +1300

    Update test classes for T19 compatibility

--- tests/external_test.php
+++ tests/external_test.php
@@ -27,6 +27,8 @@ defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
 
+require_once($CFG->dirroot . '/webservice/tests/helpers.php');
+
 /**
  * Unit tests for the webservices.
  *
@@ -35,7 +37,7 @@ global $CFG;
  * @copyright  2018 Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class mod_customcert_external_test_testcase extends advanced_testcase {
+class mod_customcert_external_test extends externallib_advanced_testcase {
 
     /**
      * Test set up.
